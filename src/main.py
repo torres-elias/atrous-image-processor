edb02263ec5e80cb5c3206d95ff223f4538874fa
Carlos Eduardo <carloscosta8001@gmail.com>
2026-02-25 17:19:25 -0300

    add: Ambiente de teste

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
-import image_io
+import numpy as np
 
-img = image_io.load("images/Shapes.png")
-print(img.shape)
+import image_io
+import atrous as at
+img = image_io.load("atrous-image-processor\\images\\Shapes.png")
+
+kernel = np.array([[1/25,1/25,1/25,1/25,1/25],
+                   [1/25,1/25,1/25,1/25,1/25],
+                   [1/25,1/25,1/25,1/25,1/25], 
+                   [1/25,1/25,1/25,1/25,1/25],
+                   [1/25,1/25,1/25,1/25,1/25]])
+img_out = at.atrous(img, kernel, 2, 1, 1)
+
+print(img_out.shape)
+
+image_io.save(img_out, path)
